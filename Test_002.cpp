b42f1dad6d9d696434aa76beede4b7e4ded70140
DBZ-HARSH <115860324+DBZ-HARSH@users.noreply.github.com>
2022-10-15 11:47:53 +0530

    Add files via upload

---
+++ Test_002.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class operations
+{
+  void add()
+  {
+      int a,b;
+      cout<<"\t\tAdditon"<<"\nEnter 2 nos\n";
+      cin>>a>>b;
+      cout<<"Result = "<<a+b<<endl;
+  }
+  
+//  void multi(){}
+  //void div(){}
+};
+int main() 
+{
+    operations z;
+    z.add();
+
+}
